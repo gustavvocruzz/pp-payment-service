d480a1f38ff8e690011adc1da9a3db2ad2ea2aef
gusttavvo <gusta.gustavocruz@gmail.com>
2026-05-25 17:29:09 -0300

    first commit

---
+++ src/test/java/com/gusttavvo/PicPayDesafio/PicPayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusttavvo.PicPayDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
