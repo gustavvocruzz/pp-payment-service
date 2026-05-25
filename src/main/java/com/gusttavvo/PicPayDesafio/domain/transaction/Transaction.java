7c5d4df1e07fcc41a0b9c6d257338d0ad5760073
gusttavvo <gusta.gustavocruz@gmail.com>
2026-05-25 17:30:31 -0300

    feat: Create Transaction entity

---
+++ src/main/java/com/gusttavvo/PicPayDesafio/domain/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package com.gusttavvo.PicPayDesafio.domain.transaction;
+
+import com.gusttavvo.PicPayDesafio.domain.users.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private BigDecimal amount;
+    private LocalDateTime timeStamp;
+}
